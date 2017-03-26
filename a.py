dcd8e8ef7d4b6f18287ed0a2c7c9e16ef6bf8ee2
Pekkle <22609050@qq.com>
2017-03-26 15:15:45 +0800

    Delete a.py

--- a.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-hello=tf.constant('hello, TensorFlow')
-sess=tf.Session()
-print sess.run(hello)
-a=tf.constant(10)
-b=tf.constant(20)
-print sess.run(a+b)
-print('Test completed!')
